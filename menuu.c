632927a8090f7de305c97555b6b43c14ff790801
cherry <csai14782@gmail.com>
2026-06-17 11:07:20 +0000

    hii

---
+++ menuu.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+int main(){
+
+    int op;
+
+    printf("!!!!!!!!Welcome to the Hotel!!!!!!!!\n");
+    printf("Select what you want -> \n");
+    printf("1.Starters\n 2.Milk shakes\n 3.Deserts\n 4.Exit\n");
+
+    
+    scanf("%d",&op);
+
+    switch(op)
+    {
+        case 1:
+        printf("You have selected starters\n");
+        break;
+
+        case 2:
+        printf("You have selected milk shakes\n");
+        break;
+
+        case 3:
+        printf("You have selected deserts\n");
+        break;
+
+        case 4:
+        printf("Thank you for visiting us\n");
+        break;
+
+        default:
+        printf("Sorry try next time!\n");
+        break;
+    }
+
+
+}
